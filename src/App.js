3f078a508644e4d6f94b4c6c4c55cbb5587a567d
eva montgomery <eva.m.montgomery@gmail.com>
2020-02-04 11:44:11 -0500

    adds hamburger/pancake toggle menu

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Nav from './Nav';
 import Footer from './Footer';
 import MainContent from './MainContent';
 
-const websiteHeader = `Amazing Web Developerer`
+const websiteHeader = `Amazing Web Developer!`
 
 const websiteCopy = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quam soluta impedit a magnam consequuntur eius id deleniti ut sequi reprehenderit ea minima dolorem consequatur, tempore eaque nemo. Maxime, fuga.`;
 
-
+const linkNames = [
+  'Home',
+  'Portfolio',
+  'Cat Pictures',
+  'Wine List',
+  'Contact'
+];
 
 function App() {
   return (
-    <React.Fragment>
+    <div className="container-fluid">
       <Header 
       title={websiteHeader}
+      />
+      <Nav 
+      links={linkNames}
       />
       <MainContent 
         content={websiteCopy}
@@ -28,7 +38,7 @@ function App() {
       )
     }  */}
       <Footer />
-    </React.Fragment>
+    </div>
   );
 }
 
---
+++ src/Nav.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class Nav extends React.Component {
+    constructor(props) {
+        // Register Nav as one of the React Components on the page.
+        super(props);
+        this.state = {
+            isOpen: false
+        };
+    }
+    
+    render () {
+        return(
+
+                this.state.isOpen ?
+                <React.Fragment>
+                <a onClick={this._toggleMenu}>🥞</a>
+                <ul>
+                    {
+                        this.props.links.map((item, index) => {
+                            return <li key={index}>{item}</li>
+                        })
+                    }
+                </ul>
+                </React.Fragment>
+                : 
+                <a onClick={this._toggleMenu}>🍔</a>
+    
+    );
+    };
+
+    _toggleMenu = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+         } , () => {
+            console.log('menu toggled!');
+       
+        });
+    }
+
+}
+
+
+export default Nav;
