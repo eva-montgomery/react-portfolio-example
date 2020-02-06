a8b99c64e5138942818cde0f57d9a6844d4942da
eva montgomery <eva.m.montgomery@gmail.com>
2020-02-06 13:50:14 -0500

    adds changes for React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
@@ -6,40 +5,75 @@ import Nav from './Nav';
 import Footer from './Footer';
 import MainContent from './MainContent';
 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 const websiteHeader = `Amazing Web Developer!`
 
 const websiteCopy = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quam soluta impedit a magnam consequuntur eius id deleniti ut sequi reprehenderit ea minima dolorem consequatur, tempore eaque nemo. Maxime, fuga.`;
 
-const linkNames = [
-  'Home',
-  'Portfolio',
-  'Cat Pictures',
-  'Wine List',
-  'Contact'
+const linkNames = [ 
+  {
+    text: 'Home',
+    path: '/'
+  },
+  {
+    text: 'Blog',
+    path: '/blog'
+  }
+
 ];
 
 function App() {
   return (
-    <div className="container-fluid">
-      <Header 
-      title={websiteHeader}
-      />
-      <Nav 
-      links={linkNames}
-      />
-      <MainContent 
-        content={websiteCopy}
-      />
-     {/* {React.createElement(
-        MainContent, 
-      {
-      'content': websiteCopy
-      }
-      )
-    }  */}
-      <Footer />
-    </div>
+
+    <Router>
+
+        <Nav 
+          links={linkNames}
+        />
+        {/* <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
+            <li><Link to="/blog/list">Blog List</Link></li>
+        </ul> */}
+        <Switch> 
+        <Route path="/blog/list"> 
+            <BlogList /> 
+          </Route>
+
+          <Route exact path="/"> 
+            <Home /> 
+          </Route>
+        
+          <Route path="/blog"> 
+            <Blog /> 
+          </Route>
+
+
+
+        </Switch>
+    </Router>
   );
+}
+
+function Home() {
+  return <h1>You are home!</h1>
+}
+
+
+function Blog() {
+  return <h1>Welcome to the blog!</h1>
+}
+
+function BlogList() {
+  return <p>This is the blog list!</p>
 }
 
 export default App;
